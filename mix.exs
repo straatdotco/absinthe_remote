4989e9f026aa668afcfe65190999ffc1d9a19de6
Luke Strickland <luke@axxim.net>
2023-08-11 10:14:06 -0400

    Adding package and PR workflow

--- mix.exs
+++ mix.exs
@@ -1,12 +1,20 @@
 defmodule AbsintheRemote.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/straatdotco/absinthe_remote"
+  @version "0.1.0"
+
   def project do
     [
       app: :absinthe_remote,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
+      name: "Absinthe Remote",
+      source_url: @source_url,
       deps: deps()
     ]
   end
@@ -21,7 +29,34 @@ defmodule AbsintheRemote.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:absinthe, "~> 1.7"}
+    ]
+  end
+
+  defp description do
+    "A library for helping you run GraphQL queries against remote GraphQL servers, with the client protections of Absinthe."
+  end
+
+  defp package do
+    [
+      description: description(),
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE.md",
+        ".formatter.exs"
+      ],
+      maintainers: [
+        "Luke Strickland"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        Website: "https://github.com/straatdotco/absinthe_remote",
+        Changelog: "#{@source_url}/blob/master/CHANGELOG.md",
+        GitHub: @source_url
+      }
     ]
   end
 end
